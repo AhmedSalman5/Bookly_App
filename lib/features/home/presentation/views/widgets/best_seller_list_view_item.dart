0b5c2b0ec9fd407a8efefe3f7d2050ea8a1cea22
AhmedSalman5 <ahmedsalman1200011@gmail.com>
2024-03-09 04:19:05 +0200

    best seller item done

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../../../../constants.dart';
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/styles.dart';
+import 'book_ratting.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -24,34 +25,40 @@ class BestSellerListViewItem extends StatelessWidget {
             ),
           ),
           const SizedBox(width: 30,),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: Text(
-                  'Harry potter and the goblet of fire',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style:Styles.textStyle20.copyWith(
-                    fontFamily: kGtSectraFine,
-                  ),
-                  ),
-              ),
-              const SizedBox(height: 3,),
-              const Text('J.K. Rowling',style: Styles.textStyle14,),
-              const SizedBox(height: 3,),
-              Row(
-                children: [
-                  Text('19.99 \$',style: Styles.textStyle20.copyWith(
-                    fontWeight: FontWeight.bold,
-                  )),
-                ],
-              ),
-            ],
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Text(
+                    'Harry potter and the goblet of fire',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style:Styles.textStyle20.copyWith(
+                      fontFamily: kGtSectraFine,
+                    ),
+                    ),
+                ),
+                const SizedBox(height: 3,),
+                const Text('J.K. Rowling',style: Styles.textStyle14,),
+                const SizedBox(height: 3,),
+                Row(
+                  children: [
+                    Text('19.99 \$',style: Styles.textStyle20.copyWith(
+                      fontWeight: FontWeight.bold,
+                    )),
+                    const Spacer(),
+                    const BookRatting(),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+
